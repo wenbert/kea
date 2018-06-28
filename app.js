34c75097aa3b67d36b2f5c106afdeea3cedd309e
wenbert <wenbert@gmail.com>
2018-06-28 21:57:03 +1200

    JSON Web Token generation

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
 const session = require('express-session');
-
-const User = require('./src/models/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(session({ secret: process.env.SECRET }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/materialize-css/dist/css')));
@@ -30,19 +33,21 @@ const nav = [
 ];
 const mainRouter = require('./src/routes/mainRoutes.js')(nav);
 const blogRouter = require('./src/routes/blogRoutes.js')(nav);
-// const loginRouter = require('./src/routes/loginRoutes.js')();
+const loginRouter = require('./src/routes/loginRoutes.js')();
 
 app.use('/', mainRouter);
 app.use('/blog', blogRouter);
+app.use('/login', loginRouter);
 
 /**
  * Create a sample user
  * Later we will query this to create JWT, etc.
+ * The passwords needs to be hashed
  */
 // app.get('/setup', (req, res) => {
 //   const user = new User({
 //     username: 'wenbert',
-//     password: 'password to hash',
+//     password: 'test',
 //     admin: true,
 //   });
 //   user.save((err) => {
--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,8 +1,36 @@
 const debug = require('debug')('app:loginController');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 function loginController() {
   function login(req, res) {
-    // Create JWT
+    const { username, password } = req.body;
+    (async function mongo() {
+      try {
+        const user = await User.findOne({ username });
+
+        // TODO check encrypted and not plain text
+        // TODO refresh token
+        if (user.password !== password) {
+          res.json({ success: false, message: 'Authentication failed.' });
+        } else {
+          const payload = {
+            admin: user.admin,
+          };
+          const token = jwt.sign(payload, process.env.SECRET, {
+            expiresIn: process.env.TOKEN_EXPIRES_IN,
+          });
+
+          res.json({
+            success: true,
+            message: 'Here is the token',
+            token,
+          });
+        }
+      } catch (err) {
+        debug(err.stack);
+      }
+    }());
   }
 
   return {
